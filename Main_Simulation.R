f9ed98eafca4d784dfe0fb873c11ac9c91a06d42
Mateusz Łącki <matteo.lacki@gmail.com>
2013-05-10 11:53:16 +0200

    Need to ponder on the strategies implementation - as subclasses or fields.

--- Main_Simulation.R
+++ Main_Simulation.R
@@ -30,5 +30,31 @@ source("./Distributions_to_check/Liang_Example.R")
 ls()
 
 
+#############################################################
+# Checking OOP.
 
+
+
+
+
+rm(list = ls())
+
+directory <- "/home/matteo/Documents/Scienza/Laurea_di_Matematica/Implementation"
+#############################################################
+getwd()
+setwd(directory)
+rm(directory)
+
+source("./objects/Loading_Objects.R")
+
+
+	# It works!
+z <- new("Parallel_Tempering_Simulations", No_of_Steps=3, Initial_Points= 5)
+Make_a_Step_of_the_Algorithm(z)
+
+zw <- new("Metropolis_Hastings_Simulations", No_of_Steps=3, Initial_Point= 5)
+Make_a_Step_of_the_Algorithm(zw)
+
+zs <- new("Parallel_Tempering_Simulations", No_of_Steps=3)
+Make_a_Step_of_the_Algorithm(zs)
 
